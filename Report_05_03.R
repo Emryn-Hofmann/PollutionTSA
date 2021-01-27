b3bfedf68d2828f064ce38799dcb9a17121c2e42
Nick-Hofmann <nickhofmann@hotmail.co.uk>
2021-01-27 13:33:00 +0000

    Selected columns of interest from the dataset, and plotted some basic graphs

--- Report_05_03.R
+++ Report_05_03.R
@@ -13,3 +13,21 @@ cor(X_Ca_Mean_corr)
 corrplot(cor(X_Ca_Mean_corr), method = "circle")
 # Might want to look at NO2 and O3, since these have the most data (no NAs)
 # Also a negative correlation between them
+
+
+X_Ca_NO2_and_O3 <- select(X_Ca, Site.Num, County.Code, Address, Date.Local, NO2.Mean,
+                   O3.Mean) # Select specific columns of interest
+X_Ca_NO2_and_O3 <- distinct(X_Ca_NO2_and_O3) # Get the distinct values, due to repetition
+# (because other pollutants had different sampling rates)
+
+#Plot the O3 and NO2 means from the various sites
+ggplot(X_Ca_NO2_and_O3, aes(x=Date.Local)) +
+  geom_line(aes(y=O3.Mean)) + 
+  facet_wrap(~Site.Num)
+  
+ggplot(X_Ca_NO2_and_O3, aes(x=Date.Local)) +
+  geom_line(aes(y=NO2.Mean)) + 
+  facet_wrap(~Site.Num)
+
+# We can see that not all sites were active across the 16 years, and that
+# the gas levels vary by site
